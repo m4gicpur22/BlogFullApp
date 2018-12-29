8e1a6d327683de7ac92152c67e776233ca5c80b8
Affner Lefevre <m4gictv@yahoo.com>
2018-12-29 07:52:34 +0000

    finished restful routes

---
+++ v1/app.js
@@ -0,0 +1,105 @@
+var bodyParser = require("body-parser"),
+methodOverride =require("method-override"),
+mongoose    = require("mongoose"),
+express     = require("express"),
+app         = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+app.get("/", function(req, res){
+   res.redirect("/blogs") 
+});
+
+
+app.get("/blogs", function(req,res){
+    
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("Error!");
+        }
+        else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.get("/blogs/new", function(req, res){
+    res.render("new"); 
+});
+
+app.post("/blogs", function(req, res){
+   Blog.create(req.body.blog, function(err, newBlog){
+       if(err){
+           res.render("new");
+       }else{
+           res.redirect("/blogs");
+       }
+   }); 
+});
+
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+/*Blog.create({
+    title: "Test Blog",
+    image: "https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60",
+    body: "HELLO THIS IS A BLOG POST!"
+});*/
+
+app.get("/blogs/:id/edit", function(req, res){
+    
+Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+        res.redirect("/blogs")
+    }
+    else{
+        res.render("edit", {blog: foundBlog});
+    }
+});
+});
+
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if(err){
+        res.redirect("/blogs");
+    }else{
+        res.redirect("/blogs/" + req.params.id);
+    }
+    
+    });
+});
+
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs");
+        }
+    })
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER IS RUNNING");
+});
